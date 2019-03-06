89e71d5ff498d95d272453fb1bc29b622ee7ce02
Mirco Tischler <mirco.tischler@gmx.de>
2019-03-06 14:18:08 +0100

    add GPL3 License

--- lib/backend/defaultValues.dart
+++ lib/backend/defaultValues.dart
@@ -1,3 +1,18 @@
+/* This file is part of Pferdepass.                                           */
+/*                                                                            */
+/* Pferdepass is free software: you can redistribute it and/or modify         */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation, either version 3 of the License, or          */
+/* (at your option) any later version.                                        */
+/*                                                                            */
+/* Pferdepass is distributed in the hope that it will be useful,              */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/* GNU General Public License for more details.                               */
+/*                                                                            */
+/* You should have received a copy of the GNU General Public License          */
+/* along with Pferdepass.  If not, see <https://www.gnu.org/licenses/>.       */
+
 const Duration defaultFarrierInterval = Duration(days: 8 * 2); // 8 weeks
 const Duration defaultInfluenzaVaccinationInterval =
     Duration(days: 182); // 1/2 year
