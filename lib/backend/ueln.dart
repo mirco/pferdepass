d73883d23e714b97344745ae2ca536e9bab3634d
Mirco Tischler <mirco.tischler@gmx.de>
2019-03-06 13:54:45 +0100

    more work on uelns

--- lib/backend/ueln.dart
+++ lib/backend/ueln.dart
@@ -1,30 +1,38 @@
-import 'package:json_annotation/json_annotation.dart';
 import 'package:flutter/material.dart' show BuildContext;
 
 import 'tools.dart';
+import 'ueln_database.dart';
 
-part 'ueln.g.dart';
+class Ueln extends Localized {
+  final String ueln;
 
-@JsonSerializable()
-class Ueln extends Localized{
-  String ueln;
+  const Ueln(this.ueln);
 
-  Ueln();
+  factory Ueln.fromString(String s) {
+    if (s.length != 14 && s.length != 15)
+      throw FormatException('Error: <$s> is not a 15 character string');
+    s = s.toUpperCase();
+    if (s.length == 14)
+      s = s.replaceRange(0, 2, '${s.substring(0, 2).toUpperCase()} ');
 
-  Ueln.fromString(this.ueln) {
-    if (!_isUeln(ueln)) throw FormatException('not a valid UELN: $ueln');
+    if (s.startsWith(RegExp('[0-9]{3}'))) {
+      final code = int.parse(s.substring(0, 3));
+      s = s.replaceRange(0, 3, uelnCountryCodeAlpha3FromNum(code));
+    }
+    if (s.startsWith(RegExp('[A-Z]{2} '))) {
+      final code = uelnCountryCodeNumFromAlpha2(s.substring(0, 2));
+      s = s.replaceRange(0, 3, uelnCountryCodeAlpha3FromNum(code));
+    }
+    return Ueln(s);
   }
-
-  factory Ueln.fromJson(Map<String, dynamic> json) => _$UelnFromJson(json);
-
-  Map<String, dynamic> toJson() => _$UelnToJson(this);
 
   String toString() => ueln;
   String toLocalizedString(BuildContext context) => ueln;
-}
 
-// only checks length for now. TODO: more validity checks for isUeln()
-bool _isUeln(String u) {
-  if (u.length != 15) return false;
-  return true;
+  static String uelnToString(Ueln u) => u.ueln;
+  static Ueln uelnFromString(String s) => Ueln.fromString(s);
+
+  bool operator ==(other) => other is Ueln && ueln == other.ueln;
+
+  int get hashCode => ueln.hashCode;
 }
