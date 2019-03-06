5902674a449a1d0a6d9fdb8080d4e3fc101bdfe2
Mirco Tischler <mirco.tischler@gmx.de>
2019-03-06 13:55:21 +0100

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pferdepass/generated/i18n.dart' as i18n;
 import 'package:pferdepass/ui/mainscreen.dart';
-
-import 'generated/i18n.dart';
 
 void main() {
   runApp(myApp());
 }
 
 Widget myApp() => MaterialApp(
-  onGenerateTitle: (BuildContext context) => S.of(context).title,
-  title: 'Pferdepass',
-  home: MainScreen(),
-  localizationsDelegates: const [
-    S.delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ],
-  supportedLocales: S.delegate.supportedLocales,
-);
+      onGenerateTitle: (BuildContext context) => i18n.S.of(context).title,
+      home: MainScreen(),
+      localizationsDelegates: const [
+        i18n.S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: i18n.S.delegate.supportedLocales,
+    );
