cb138a4bfd01741735ede15d0551eb6668a1e2d0
Mirco Tischler <mirco.tischler@gmx.de>
2019-03-06 13:57:26 +0100

    add default values

---
+++ lib/backend/defaultValues.dart
@@ -0,0 +1,5 @@
+const Duration defaultFarrierInterval = Duration(days: 8 * 2); // 8 weeks
+const Duration defaultInfluenzaVaccinationInterval =
+    Duration(days: 182); // 1/2 year
+const Duration defaultTetanusVaccinationInterval =
+    Duration(days: 2 * 365); // 2 years
