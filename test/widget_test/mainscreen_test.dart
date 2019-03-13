b66e76d11fb2ecc8816de22b298ec5a4e97c2093
Mirco Tischler <mirco.tischler@gmx.de>
2019-03-13 02:17:12 +0100

    test: add a first version of mainscreen widget test

---
+++ test/widget_test/mainscreen_test.dart
@@ -0,0 +1,38 @@
+/* This file is part of Pferdepass.                                           */
+/*                                                                            */
+/* Pferdepass is free software: you can redistribute it and/or modify         */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation, either version 3 of the License, or          */
+/* (at your option) any later version.                                        */
+/*                                                                            */
+/* Pferdepass is distributed in the hope that it will be useful,              */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/* GNU General Public License for more details.                               */
+/*                                                                            */
+/* You should have received a copy of the GNU General Public License          */
+/* along with Pferdepass.  If not, see <https://www.gnu.org/licenses/>.       */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pferdepass/generated/i18n.dart' as i18n;
+import 'package:pferdepass/ui/mainscreen.dart';
+
+void main() {
+  group('MainScreen', () {
+    setUp(() {});
+    testWidgets('constructor test', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: MainScreen(),
+        localizationsDelegates: const [
+          i18n.S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: i18n.S.delegate.supportedLocales,
+      ));
+      expect(find.byType(ListView), findsOneWidget);
+    });
+  });
+}
