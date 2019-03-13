5fef5683fbf28819f75355a6044b89ccaa319886
Mirco Tischler <mirco.tischler@gmx.de>
2019-03-14 00:55:51 +0100

    travis: rework CI tests
    Execute unit and widget tests in parralel and try integration tests in a separate stage

---
+++ test/integration_test/app_test.dart
@@ -0,0 +1,36 @@
+/* This file is part of Pferdepass.                                           */
+/*                                                                            */
+/* Pferdepass is free software: you can redistribute it and/or modify         */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation, either version 3 of the License, or          */
+/* (at your option) any later version.                                        */
+/*                                                                            */
+/* Pferdepass is distributed in the hope that it will be useful,              */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/* GNU General Public License for more details.                               */
+/*                                                                            */
+/* You should have received a copy of the GNU General Public License          */
+/* along with Pferdepass.  If not, see <https://www.gnu.org/licenses/>.       */
+
+// Imports the Flutter Driver API
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Pferdepass', () {
+    FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+  });
+}
